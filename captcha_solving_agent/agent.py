94aa80dcf0fd3ad464c2318e72c8fed85a32c209
Shishir Aravindan <shishir.aravindan@gmail.com>
2024-07-25 00:47:22 +0530

    extracts captcha form information

--- captcha_solving_agent/agent.py
+++ captcha_solving_agent/agent.py
@@ -3,49 +3,82 @@
 import utils
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import logging
 import logging.config
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from decaptcha import decaptcha
 
 config = {
     "css_target": ".rc-imageselect-desc-no-canonical > strong:nth-child(1)",
-    "x_path": "/html/body/div/div/div[2]/div[1]/div[1]/div/strong"
+    "x_path": "/html/body/div/div/div[2]/div[1]/div[1]/div/strong",
+    "iframe_id": "/html/body/div/div[2]/iframe",
+    "instructions_CSS_target": "strong:nth-child(1)",
+    "images_CSS": "html body div div#rc-imageselect div.rc-imageselect-payload div.rc-imageselect-challenge div#rc-imageselect-target.rc-imageselect-target table.rc-imageselect-table-33",
+    "all_images": "html body div div#rc-imageselect div.rc-imageselect-payload div.rc-imageselect-challenge div#rc-imageselect-target.rc-imageselect-target table.rc-imageselect-table-33 tbody tr td.rc-imageselect-tile div.rc-image-tile-target div.rc-image-tile-wrapper img.rc-image-tile-33"
 }
 
 def solve_captcha(driver: WebDriver):
-    # targetElement = driver.find_element(By.CSS_SELECTOR, config["css_target"])
+    payload, driver = identify_and_extract_captcha_form(driver)
+    if not payload: 
+        return driver
+    else:
+        target_class, images = payload[0], payload[1]
+        img_path = utils._download_image_locally(images)
+        decaptcha(target_class, img_path)
+
+def identify_and_extract_captcha_form(driver: WebDriver):
     try:
-        targetElement = WebDriverWait(driver, 20).until(
-            # EC.presence_of_element_located((By.CSS_SELECTOR, config["css_target"]))
-            EC.presence_of_element_located((By.XPATH, config["x_path"]))
+        captcha_iframe = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, config["iframe_id"]))
         )
-        logging.info(f"solve_captcha: {targetElement}")
-    # try:
-    #     targetElement = driver.find_element(By.XPATH, config["x_path"])
-    #     logging.info(f"solve_captcha: target={targetElement.text}")
+        logging.info(f"CAPTCHA iFrame identified. Extracting info ...")
+        payload, driver = extract_captcha_details(driver, captcha_iframe)
+        driver.switch_to.default_content()
+        return payload, driver
     except NoSuchElementException as e:
         logging.error(f"solve_captcha exception: {e.msg}")
-    
+    except TimeoutException as e:
+        logging.info(f"No CAPTCHA found on page {e.msg}")
+    return None, driver
 
-
-def identify_captcha_form(driver: WebDriver):
-    captcha_form_element = driver.find_element(By.CSS_SELECTOR, '.rc-imageselect')
+def extract_captcha_details(driver: WebDriver, captcha_iframe: WebElement):
     try:
-        instructions_element = captcha_form_element.find_element(By.CSS_SELECTOR, "rc-imageselect-desc-no-canonical")
-        print(instructions_element.text)
+        driver.switch_to.frame(captcha_iframe)
+        target_class, driver = _get_target_class(driver)
+        images_src, driver = _get_images(driver)
+        return [target_class, images_src], driver
     except Exception as e:
-        print("An error occurred:", str(e))
+        logging.error(f"solve_captcha exception: {e}")
+        driver.switch_to.default_content()
+    return None, driver
 
-# def extract_captcha_details():
-#     pass
+def _get_target_class(driver):
+    try:
+        instruction_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, config["instructions_CSS_target"]))
+        )
+        logging.info(f"CAPTCHA target: {instruction_element.text}")
+        return instruction_element.text, driver
+    except Exception as e:
+        logging.error(f"Error Processing CAPTCHA form's instructions: {e}")
+    return None, driver
 
-# def call_decaptcha():
-#     pass
-
-# def solve_captcha_form():
-#     pass
+def _get_images(driver):
+    try:
+        images_table_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, config["all_images"]))
+        )
+        images_src = images_table_element.get_attribute("src")
+        logging.info(f"CAPTCHA image source: {images_src}")
+        return images_src, driver
+    except NoSuchElementException as e:
+        logging.error(f"[NoSuchElementException] Error Processing CAPTCHA form's images: {e}")
+    except TimeoutException as e:
+        logging.error(f"[TimeoutException] Error Processing CAPTCHA form's images: {e}")
+    return '', driver
 
 #--------------------------------------------------------------------------------
 # Test code
