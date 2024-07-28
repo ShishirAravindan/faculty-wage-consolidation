071eb1e39d2b1b69a717251bc771367c9f87d4a6
Shishir Aravindan <shishir.aravindan@gmail.com>
2024-07-28 16:44:38 +0530

    completed captcha workflow

--- captcha_solving_agent/agent.py
+++ captcha_solving_agent/agent.py
@@ -10,34 +10,37 @@ import logging
 import logging.config
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from decaptcha import decaptcha
+import time
 
 config = {
-    "css_target": ".rc-imageselect-desc-no-canonical > strong:nth-child(1)",
-    "x_path": "/html/body/div/div/div[2]/div[1]/div[1]/div/strong",
-    "iframe_id": "/html/body/div/div[2]/iframe",
-    "instructions_CSS_target": "strong:nth-child(1)",
-    "images_CSS": "html body div div#rc-imageselect div.rc-imageselect-payload div.rc-imageselect-challenge div#rc-imageselect-target.rc-imageselect-target table.rc-imageselect-table-33",
-    "all_images": "html body div div#rc-imageselect div.rc-imageselect-payload div.rc-imageselect-challenge div#rc-imageselect-target.rc-imageselect-target table.rc-imageselect-table-33 tbody tr td.rc-imageselect-tile div.rc-image-tile-target div.rc-image-tile-wrapper img.rc-image-tile-33"
+    "iframe": "/html/body/div/div[2]/iframe",
+    "captcha_instructions": "strong:nth-child(1)",
+    "captcha_image_src": "img.rc-image-tile-33",
+    "image_grid": "div.rc-image-tile-wrapper",
+    "verify_button": "#recaptcha-verify-button" 
 }
 
-def solve_captcha(driver: WebDriver):
-    payload, driver = identify_and_extract_captcha_form(driver)
-    if not payload: 
-        return driver
-    else:
+def captcha_workflow(driver: WebDriver):
+    payload, driver = _identify_and_extract_captcha_form(driver)
+    if payload:
         target_class, images = payload[0], payload[1]
         img_path = utils._download_image_locally(images)
-        results = decaptcha(target_class, img_path)
-        click_images(driver, results)
+        _solve_captcha(driver, target_class, img_path)
+    return driver
 
-def identify_and_extract_captcha_form(driver: WebDriver):
+
+def _solve_captcha(driver: WebDriver, target_class: str, img_path: str):
+    captcha_predictions = decaptcha(target_class, img_path)
+    _click_images(driver, captcha_predictions)
+    _complete_form(driver)
+    driver.switch_to.default_content()
+
+def _identify_and_extract_captcha_form(driver: WebDriver):
     try:
         captcha_iframe = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, config["iframe_id"]))
+            EC.presence_of_element_located((By.XPATH, config["iframe"]))
         )
-        logging.info(f"CAPTCHA iFrame identified. Extracting info ...")
-        payload, driver = extract_captcha_details(driver, captcha_iframe)
-        driver.switch_to.default_content()
+        payload, driver = _extract_captcha_details(driver, captcha_iframe)
         return payload, driver
     except NoSuchElementException as e:
         logging.error(f"solve_captcha exception: {e.msg}")
@@ -45,7 +48,7 @@ def identify_and_extract_captcha_form(driver: WebDriver):
         logging.info(f"No CAPTCHA found on page {e.msg}")
     return None, driver
 
-def extract_captcha_details(driver: WebDriver, captcha_iframe: WebElement):
+def _extract_captcha_details(driver: WebDriver, captcha_iframe: WebElement):
     try:
         driver.switch_to.frame(captcha_iframe)
         target_class, driver = _get_target_class(driver)
@@ -59,8 +62,9 @@ def extract_captcha_details(driver: WebDriver, captcha_iframe: WebElement):
 def _get_target_class(driver):
     try:
         instruction_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, config["instructions_CSS_target"]))
+            EC.presence_of_element_located((By.CSS_SELECTOR, config["captcha_instructions"]))
         )
+        logging.info(f"CAPTCHA iFrame identified. Extracting info ...")
         logging.info(f"CAPTCHA target: {instruction_element.text}")
         return instruction_element.text, driver
     except Exception as e:
@@ -70,7 +74,7 @@ def _get_target_class(driver):
 def _get_images(driver):
     try:
         images_table_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, config["all_images"]))
+            EC.presence_of_element_located((By.CSS_SELECTOR, config["captcha_image_src"]))
         )
         images_src = images_table_element.get_attribute("src")
         logging.info(f"CAPTCHA image source: {images_src}")
@@ -81,7 +85,7 @@ def _get_images(driver):
         logging.error(f"[TimeoutException] Error Processing CAPTCHA form's images: {e}")
     return '', driver
 
-def click_images(driver: WebDriver, results: list[int]):
+def _click_images(driver: WebDriver, preds: list[int]) -> WebDriver:
     """Given the driver and the results (i.e. predictions from the vLLM) click
     the corresponding tiles in the captcha form.
 
@@ -90,7 +94,29 @@ def click_images(driver: WebDriver, results: list[int]):
     should click the middle, middle-right, bottom-left and bottom-right
     then should hit the verify button
     """
-    pass
+    try:
+        image_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, config["image_grid"]))
+        )
+        for i, pred in enumerate(preds):
+            if pred == True: image_elements[i].click()
+    except NoSuchElementException as e:
+        print(f"Error: Could not find image elements with selector '{config['image_grid']}'. {e}")
+    except Exception as e:
+        print(f"Error: Could not click the image elements. {e}")
+    return driver
+
+def _complete_form(driver: WebDriver) -> WebDriver:
+    try:
+        verify_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, config["verify_button"]))
+        )
+        verify_button.click()
+    except NoSuchElementException as e:
+        print(f"Error: Could not find the verify button with selector '{config['verify_button']}'. {e}")
+    except Exception as e:
+        print(f"Error: Could not click the verify button. {e}")
+    return driver
 
 #--------------------------------------------------------------------------------
 # Test code
@@ -101,7 +127,7 @@ def test_main():
     logging.info("PAGE LOADING")
     driver = utils.fill_form(URL, name)
     logging.info("FORM FILLED")
-    solve_captcha(driver)
+    captcha_workflow(driver)
     logging.info("CAPTCHA reached")
 
     # TODO: All other test case
