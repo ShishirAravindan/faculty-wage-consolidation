4b5170edf0958d6cb2542efcbc12a3590c830a87
AkhilChoraria <chorariaakhil03@gmail.com>
2024-08-04 12:36:37 -0400

    Modified the update and write state file method

--- state.py
+++ state.py
@@ -21,23 +21,32 @@ def get_fresh_chunk(state_file_path, chunk_size):
     with open(state_file_path, 'r') as json_file:
         state_data = json.load(json_file)
 
-    chunk = []
+    chunk = {}
     null_found = False
     for entry in state_data:
-        if entry['faculty'] is None:
+        if entry['Department'] is None:
             if not null_found: null_found = True
-            chunk.append(entry)
+            chunk[entry['Name']] = None
             if len(chunk) == chunk_size: break
         elif null_found:
             continue
-
     return chunk
 
 
 def update_state_file(state_file_path, new_data):
-    pass
+    with open(state_file_path, 'r') as json_file:
+        state_data = json.load(json_file)
+
+    for entry in state_data:
+        if entry['Name'] in new_data:
+            entry['Department'] = new_data[entry['Name']]
+
+    with open(state_file_path, 'w') as json_file:
+        json.dump(state_data, json_file, indent=2)
+        print(f"JSON data has been updated in {state_file_path}")
 
 
 #make_initial_state_file('2_salaries/UoI/uoi_2023.xlsx', '2_salaries/UoI/state_2023.json')
-make_initial_state_file('../IOWAsalary1993-2023upd.xlsx', '2_salariesUoIstate_2023.json')
-
+#make_initial_state_file('../IOWAsalary1993-2023upd.xlsx', '2_salariesUoIstate_2023.json')
+# chunk = get_fresh_chunk('data/2_salariesUoIstate_2023.json', 5)
+# print(chunk)
